d87b6f5453a87ef895c1b0b2745806a10c7f1852
Doxifieq <94010949+Doxifieq@users.noreply.github.com>
2021-11-12 14:35:34 +0100

    edits

--- Main.lua
+++ Main.lua
@@ -7,14 +7,16 @@ local Humanoid = Character:FindFirstChild("Humanoid")
 local Root = Character:FindFirstChild("HumanoidRootPart")
 local Prefix = "@"
 
-Player.Chatted:Connect(function(C)
-	if C:match(Prefix .. "iy") or C:match(Prefix .. "infyield") or C:match(Prefix .. "infiniteyield") then
+Player.Chatted:Connect(function(Chat)
+	if Chat == Prefix .. "iy" or Prefix .. "infyield" or Prefix .. "infiniteyield" then
 		loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
 
-	elseif C:match(Prefix .. "rj") or C:match(Prefix .. "rejoin") then
+
+	elseif Chat == Prefix .. "rj" or Prefix .. "rejoin" or then
 		TeleportService:Teleport(game.PlaceId)
 
-	elseif C:match(Prefix .. "god") then
+
+	elseif Chat == Prefix .. "gm" or Prefix .. "god" or Prefix .. "godmode" then
 		local Stats = Character:FindFirstChild("Stats")
 		if Stats:FindFirstChild("Phys-Resist") then
 			Stats:WaitForChild("Phys-Resist"):Destroy()
@@ -23,21 +25,27 @@ Player.Chatted:Connect(function(C)
 			Humanoid.Health = 0 --only way to undo godmode
 		end
 
-	elseif C:match(Prefix .. "reset") or C:match(Prefix .. "hardreset") or C:match(Prefix .. "hr") then
+
+	elseif Chat == Prefix .. "hr" or Prefix .. "reset" or Prefix .. "hardreset" then
 		Humanoid.Health = 0
 
-	elseif C:match(Prefix .. "telespeed") or C:match(Prefix .. "speed") then
+
+	elseif Chat == Prefix .. "ts" or Prefix .. "speed" or "telespeed" then
+		loadstring(game:HttpGet('https://raw.githubusercontent.com/Doxifieq/Dragon-Ball-Z-Final-Stand/main/TeleSpeed.lua'))()
 		StarterGui:SetCore("SendNotification", {
 			Title = "Commands",
 			Text = "Press V to toggle telespeed"
 		})
-		loadstring(game:HttpGet('https://raw.githubusercontent.com/Doxifieq/Dragon-Ball-Z-Final-Stand/main/TeleSpeed.lua'))()
 
-	elseif C:match(Prefix .. "chatlogs") or C:match(Prefix .. "cl") then
+
+	elseif Chat == Prefix .. "cl" or Prefix .. "chat" or "chatlogs" then
 		if Player.PlayerGui.Chat.Frame.ChatChannelParentFrame.Visible == false then
 			Player.PlayerGui.Chat.Frame.ChatChannelParentFrame.Visible = true
 		else
 			Player.PlayerGui.Chat.Frame.ChatChannelParentFrame.Visible = false
 		end
+
+
+	--elseif
 	end
 end)
